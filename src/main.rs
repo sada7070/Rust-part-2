9a7dd5231ef2543f90e11a2117c76747f41fa733
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-07-12 17:31:03 +0530

    Vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// vectors(arrays)
+
+// funtion which filter and returns even values of the given vector
 fn main() {
-    println!("Hello, world!");
+    let vec = vec![1, 2, 3, 4];
+
+    println!("{:?}", vec);
+    println!("Even elements of the vector: {:?}", filter_vec(vec));
+}
+
+fn filter_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut new_vec = Vec::new();
+
+    for val in vec {
+        if val % 2 == 0 {
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec;
 }
